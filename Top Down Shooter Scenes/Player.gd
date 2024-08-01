992fcf1ef4cc415c2d18e488b0abbfb21644fd79
SHADOW-BORN <rivmor65@kingshigh.school.nz>
2024-08-02 09:01:23 +1200

    Shooting Function in Progress

--- Top Down Shooter Scenes/Player.gd
+++ Top Down Shooter Scenes/Player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
+#Variables
 @onready var world = get_node('/root/World')
 @export var SPEED = 100
 @export var ACCELERATION = 20
 @export var FRICTION = 10
+var direction_facing = Vector2.ZERO
 const BULLET = preload("res://Bullet.tscn")
 @warning_ignore("unused_parameter")
+#Functions
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
-	#rotate(get_angle_to(get_global_mouse_position()))
+	#Movement
 	var direction = Input.get_vector("left","right", "up", "down").normalized()
 	if direction :
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
@@ -15,7 +18,10 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 		
 	move_and_slide()
-func _on_bullet_timer_timeout():
-	var bullet = BULLET.instantiate()
-	bullet.global_position = global_position
-	world.add_child(bullet)
+	
+#Bullet Code
+	if Input.is_action_just_pressed("shoot"):
+		var bullet = BULLET.instantiate()
+		bullet.rotate(direction.angle())
+		bullet.global_position = global_position
+		world.add_child(bullet)
